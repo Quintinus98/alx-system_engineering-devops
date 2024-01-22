ac57090b00d80295692a13f7e0dc9da17462dcb6
David <obododavidsop@gmail.com>
2024-01-22 02:59:06 +0100

    100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,10 +1,10 @@
 # using Puppet to make changes to our configuration file
 file { '~/.ssh/config':
-  ensure => file,
+  ensure  => file,
   content => "
               HOST *
               PasswordAuthentication no
               IdentityFile ~/.ssh/school
             ",
-  mode => '0600',
+  mode    => '0600',
 }
