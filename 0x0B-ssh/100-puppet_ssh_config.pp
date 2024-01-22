743a912b6b3a91529b81829385806641e2630031
David <obododavidsop@gmail.com>
2024-01-22 03:20:29 +0100

    updated 100

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,9 +1,17 @@
 # using Puppet to make changes to our configuration file
-file { ' /etc/ssh/ssh_config':
-  ensure  => file,
-  content => "
-              HOST *
-                PasswordAuthentication no
-                IdentityFile  ~/.ssh/school
-            ",
+include stdlib
+
+
+file_line { 'Use Identity file':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    IdentityFile  ~/.ssh/school',
+  replace => true,
+}
+
+file_line { 'No password auth':
+  ensure  => present,
+  path    => '/etc/ssh/ssh_config',
+  line    => '    PasswordAuthentication no',
+  replace => true,
 }
