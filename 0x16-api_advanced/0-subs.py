f02db2cfa2f08d516f6b9e6d36c79e82a4d8e326
David <obododavidsop@gmail.com>
2024-03-06 00:34:06 +0100

    updated 0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,7 +7,8 @@ def number_of_subscribers(subreddit):
     """Number of subscribers"""
     if subreddit is None or type(subreddit) is not str:
         return (0)
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {'User-Agent': '0x16-api_advanced'}
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0 \
+               (by /u/firdaus_cartoon_jr)'}
     response = requests.get(url, headers=headers, allow_redirects=False)
     return response.json().get('data', {}).get('subscribers', 0)
