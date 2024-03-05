8f71742e6219a346dc7ff8703ef4531e11f385f1
David <obododavidsop@gmail.com>
2024-03-06 00:36:13 +0100

    0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -6,9 +6,10 @@ import requests
 def number_of_subscribers(subreddit):
     """Number of subscribers"""
     if subreddit is None or type(subreddit) is not str:
-        return (0)
+        return 0
     url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
     headers = {'User-Agent': '0x16-api_advanced:project:v1.0.0 \
                (by /u/firdaus_cartoon_jr)'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    return response.json().get('data', {}).get('subscribers', 0)
+    response = requests.get(url, headers=headers)
+    total_subs = response.json().get('data', {}).get('subscribers', 0)
+    return total_subs
