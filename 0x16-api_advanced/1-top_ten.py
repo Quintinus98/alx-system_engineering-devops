fc26e782e331b38860cd35b24c79198d01049277
David <obododavidsop@gmail.com>
2024-03-06 01:15:23 +0100

    updated 1-top_ten

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -7,7 +7,7 @@ def top_ten(subreddit):
     """Top ten function"""
     url = f"https://www.reddit.com/r/{subreddit}/hot.json"
     headers = {'User-Agent': 'Mozilla/5.0'}
-    params = {'limit': 10}
+    params = {'limit': 9}
     response = requests.get(url, headers=headers, params=params,
                             allow_redirects=False)
 
