1cef583bed134f3f80f6becc8aaf755846d093a0
David <obododavidsop@gmail.com>
2024-03-17 02:51:16 +0100

    updated all

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,7 +1,7 @@
 # testing how well our web server setup featuring
 # Nginx is doing under pressure
 
-exec { 'update_ulimit_variable':
+exec { 'ufix--for-nginx':
   command => 'sed -i s/"-n 15"/"-n 4096"/g /etc/default/nginx',
   path    => '/usr/local/bin/:/bin/'
 }
